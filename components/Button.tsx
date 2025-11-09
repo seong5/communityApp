7836ae10a3c4bb0a92ee25281ab0ddb09e2ec451
seong5 <greenbi0852@gmail.com>
2025-11-10 01:13:25 +0900

    버튼컴포넌트생성

---
+++ components/Button.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Pressable, StyleSheet, Text } from 'react-native'
+
+function Button({}) {
+  return (
+    <Pressable>
+      <Text>버튼</Text>
+    </Pressable>
+  )
+}
+
+const buttonStyles = StyleSheet.create({})
+
+export default Button
