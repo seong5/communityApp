201d6b932f4507bc4aff824a41c6b15b051f3a5f
seong5 <greenbi0852@gmail.com>
2025-11-10 06:02:19 +0900

    네비게이션 경로 지정 및 아이콘 렌더링

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,6 @@
 import { colors } from '@/constants/colors'
+import Ionicons from '@expo/vector-icons/Ionicons'
+import Octicons from '@expo/vector-icons/Octicons'
 import { Tabs } from 'expo-router'
 import React from 'react'
 
@@ -14,18 +16,27 @@ export default function TabLayout() {
         name="index"
         options={{
           title: '홈',
+          tabBarIcon: ({ color, focused }) => (
+            <Octicons name={focused ? 'home-fill' : 'home'} size={26} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="myProfile"
         options={{
-          title: '내 프로필',
+          title: '프로필',
+          tabBarIcon: ({ color, focused }) => (
+            <Octicons name={focused ? 'person-fill' : 'person'} size={26} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="setting"
         options={{
           title: '설정',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'settings' : 'settings-outline'} size={26} color={color} />
+          ),
         }}
       />
     </Tabs>
