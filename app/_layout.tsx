1ea4fafe1bfbad1d44ba9ec97c6e261b498d40d3
seong5 <greenbi0852@gmail.com>
2025-11-10 00:32:13 +0900

    프로젝트 세팅

---
+++ app/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from 'expo-router'
+import 'react-native-reanimated'
+
+export const unstable_settings = {
+  anchor: '(tabs)',
+}
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+    </Stack>
+  )
+}
