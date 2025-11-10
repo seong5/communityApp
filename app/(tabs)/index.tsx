2fdf8f958888dca9e7e8a3f55f032ba6241d488e
seong5 <greenbi0852@gmail.com>
2025-11-10 15:24:12 +0900

    불필요한 코드 삭제

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,3 @@
-import Button from '@/components/Button'
-import Input from '@/components/Input'
 import React from 'react'
 import { Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
@@ -7,11 +5,7 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 export default function HomeScreen() {
   return (
     <SafeAreaView>
-      <Text>테스트</Text>
-      <Button label="버튼" variant="primary" size="lg" onPress={() => {}} />
-      <Input label="테스트인풋" placeholder="테스트입니다" variant="primary" />
-      <Input label="테스트인풋2" placeholder="테스트입니다2" variant="second" />
-      <Input label="테스트인풋3" placeholder="테스트입니다3" variant="third" />
+      <Text>홈화면</Text>
     </SafeAreaView>
   )
 }
