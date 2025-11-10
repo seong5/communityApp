260ec1f4ae49668a01c177f4e7d71286fd6f2bff
seong5 <greenbi0852@gmail.com>
2025-11-10 16:45:53 +0900

    프로필 기본 이미지 저장

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,5 +1,8 @@
-import { View } from 'react-native'
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
 
 export default function Profile() {
   return <View></View>
 }
+
+const styles = StyleSheet.create({})
