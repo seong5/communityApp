704c16fc687a9796449631c2e19e5da15c6a3325
seong5 <greenbi0852@gmail.com>
2025-11-10 15:21:24 +0900

    auth 폴더 생성 후 index.tsx _layout.tsx 생성

---
+++ app/auth/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+export default function AuthScreen() {
+  return (
+    <SafeAreaView>
+      <Text>로그인홈</Text>
+    </SafeAreaView>
+  )
+}
