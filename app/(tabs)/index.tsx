3ec3f6e586de85090f3d0c2f8d1155a7b2f4ef35
seong5 <greenbi0852@gmail.com>
2025-11-12 02:48:17 +0900

    로그아웃 버튼 홈으로 이동

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,32 @@
 import Button from '@/components/common/Button'
 import FeedList from '@/components/FeedList'
+import { useAuthRequireQuery } from '@/hooks/useAuthRequireQuery'
+import { useLogout } from '@/hooks/useLogout'
 import { router } from 'expo-router'
 import React from 'react'
+import { StyleSheet, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function HomeScreen() {
+  useAuthRequireQuery()
+
+  const { mutate: logout, isPending } = useLogout()
+
   return (
     <SafeAreaView>
-      <Button label="로그인/회원가입" size="md" onPress={() => router.push('/auth')} />
-      <Button label="글쓰기" size="md" onPress={() => router.push('/posting/PostFeed')} />
+      <View style={styles.container}>
+        <Button label="로그인/회원가입" size="md" onPress={() => router.push('/auth')} />
+        <Button label="로그아웃" size="md" onPress={() => logout()} disabled={isPending} />
+        <Button label="글쓰기" size="md" onPress={() => router.push('/posting/PostFeed')} />
+      </View>
       <FeedList />
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+})
