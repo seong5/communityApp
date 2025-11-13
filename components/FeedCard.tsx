11e5d4d09ee82b8ac8a3f42f0f8d93ddbe28914a
seong5 <greenbi0852@gmail.com>
2025-11-13 19:05:01 +0900

    error 변수 제거

--- components/FeedCard.tsx
+++ components/FeedCard.tsx
@@ -93,7 +93,7 @@ export default function FeedCard({ feed, isDetail = false, onLikePress }: FeedCa
       const link = `communityapp://posting/${feed.id}`
       await Clipboard.setStringAsync(link)
       Alert.alert('링크 복사됨', '피드 링크가 클립보드에 복사되었습니다.')
-    } catch (error) {
+    } catch {
       Alert.alert('복사 실패', '링크 복사에 실패했습니다.')
     }
   }
