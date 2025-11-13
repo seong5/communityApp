3356414d390cf167f177f8e35eb45da37028b8ba
seong5 <greenbi0852@gmail.com>
2025-11-13 17:39:50 +0900

    로고 이미지 임의로 생성 및 적용

--- app/auth/index.tsx
+++ app/auth/index.tsx
@@ -1,24 +1,50 @@
 import Button from '@/components/common/Button'
 import { Link, router } from 'expo-router'
-import { StyleSheet, View } from 'react-native'
+import { Image, ScrollView, StyleSheet, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function AuthScreen() {
   return (
-    <SafeAreaView>
-      <View style={styles.buttonContainer}>
-        <Button label="이메일로 로그인하기" onPress={() => router.push('/auth/login')} />
-        <Link href={'/auth/signup'} style={styles.signup}>
-          이메일로 회원가입
-        </Link>
-      </View>
+    <SafeAreaView style={styles.safeArea}>
+      <ScrollView contentContainerStyle={styles.scrollContent}>
+        <View style={styles.logoContainer}>
+          <Image
+            source={require('../../assets/images/main-logo.png')}
+            style={styles.logo}
+            resizeMode="contain"
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button label="이메일로 로그인하기" onPress={() => router.push('/auth/login')} />
+          <Link href={'/auth/signup'} style={styles.signup}>
+            이메일로 회원가입
+          </Link>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+  scrollContent: {
+    flexGrow: 1,
+    justifyContent: 'center',
+  },
+  logoContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 100,
+  },
+  logo: {
+    width: 500,
+    height: 350,
+  },
   buttonContainer: {
     paddingHorizontal: 20,
+    gap: 10,
   },
   signup: {
     marginTop: 10,
